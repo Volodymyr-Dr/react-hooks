176f5b62a1ab84008330f20924bcdea5c7ef9723
Volodymyr Pavlyk <vv.dr.pvlk@gmail.com>
2021-08-07 14:02:03 +0300

    created hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import contacts from './components/ContactsList'
+import Contact from './components/Contact'
 import './App.css';
 
+
 function App() {
-  return (
+  const [contact,  setContact] = useState(contacts);
+  
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    const name = contacts.filter(person =>
+      person.lastName.toLowerCase().includes(searchTerm.toLowerCase())
+      || person.firstName.toLowerCase().includes(searchTerm.toLowerCase())
+      || person.phone.toLowerCase().includes(searchTerm.toLowerCase()));
+    
+    setContact(name)
+  }, [searchTerm]);
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value)
+  };
+ 
+  // const getGender = () => {
+  //   if (document.getElementById("male").checked) {
+  //     setContact(contact.filter(item => item.gender === "https://image.flaticon.com/icons/png/512/1340/1340619.png"));
+  //   }
+  //   if (document.getElementById("female").checked) {
+  //     setContact(contact.filter(item => item.gender === "https://image.flaticon.com/icons/png/512/766/766514.png"));
+  //   }
+  //   if (document.getElementById("undefined").checked) {
+  //     setContact(contact.filter(item => item.gender === "http://pngimg.com/uploads/question_mark/question_mark_PNG91.png"));
+  //   }
+  // };
+  
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Contacts</h1>
+       <div>
+         <input 
+            type="text"  
+            placeholder="Search"
+            value={searchTerm}
+            onChange={handleSearchChange}
+          />
+       </div>
+       {/* <>Male<input type="checkbox" id="male" onChange={getGender}/></>
+       <>Female<input type="checkbox" id="female" onChange={getGender}/></>
+       <>Undefined<input type="checkbox" id="undefined" onChange={getGender}/></> */}
+       <ul>
+            {contact.map(item => <Contact key={item.id} {...item} />)}
+        </ul>
     </div>
   );
 }
---
+++ src/components/Contact.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+export default function Contact ({id, firstName, lastName, phone, gender}) {
+    return(
+        <li>
+        <div className="name">
+            <span className="firstName">{firstName}&emsp;</span>
+            
+            <span className="lastName">{lastName}</span>
+            <span className="gender"><img src={gender} alt=""></img></span>
+            <div className="phone">{phone}</div>
+        </div>
+        
+        
+    </li>
+    )
+}
