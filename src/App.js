6cfc01a71fccfe4c06d5c13bbf889735db6b84b6
Volodymyr-Dr <vv.dr.pvlk@gmail.com>
2021-08-26 14:26:02 +0300

    created some mistakes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React, {useState, useEffect} from 'react';
-import contacts from './components/ContactsList'
-import Contact from './components/Contact'
+
+import contacts from './Components/ContactsList'
+import Contact from './Components/Contact'
+
 import './App.css';
 
 
 function App() {
   const [contact,  setContact] = useState(contacts);
-  
   const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
+    function cheking (e) {
+      return e.toLowerCase().includes(searchTerm.toLowerCase())
+    }
     const name = contacts.filter(person =>
-      person.lastName.toLowerCase().includes(searchTerm.toLowerCase())
-      || person.firstName.toLowerCase().includes(searchTerm.toLowerCase())
-      || person.phone.toLowerCase().includes(searchTerm.toLowerCase()));
-    
+      cheking(person.lastName) || cheking(person.firstName) || cheking(person.phone));
+
     setContact(name)
   }, [searchTerm]);
 
@@ -22,17 +24,17 @@ function App() {
     setSearchTerm(event.target.value)
   };
  
-  // const getGender = () => {
-  //   if (document.getElementById("male").checked) {
-  //     setContact(contact.filter(item => item.gender === "https://image.flaticon.com/icons/png/512/1340/1340619.png"));
-  //   }
-  //   if (document.getElementById("female").checked) {
-  //     setContact(contact.filter(item => item.gender === "https://image.flaticon.com/icons/png/512/766/766514.png"));
-  //   }
-  //   if (document.getElementById("undefined").checked) {
-  //     setContact(contact.filter(item => item.gender === "http://pngimg.com/uploads/question_mark/question_mark_PNG91.png"));
-  //   }
-  // };
+  const getGender = () => {
+    if (document.getElementById("male").checked) {
+      setContact(contact.filter(item => item.gender === "https://image.flaticon.com/icons/png/512/1340/1340619.png"));
+    }
+    if (document.getElementById("female").checked) {
+      setContact(contact.filter(item => item.gender === "https://image.flaticon.com/icons/png/512/766/766514.png"));
+    }
+    if (document.getElementById("undefined").checked) {
+      setContact(contact.filter(item => item.gender === "http://pngimg.com/uploads/question_mark/question_mark_PNG91.png"));
+    }
+  };
   
     return (
     <div className="App">
@@ -46,9 +48,9 @@ function App() {
           />
        </div>
 
-       {/* <>Male<input type="checkbox" id="male" onChange={getGender}/></>
+       <>Male<input type="checkbox" id="male" onChange={getGender}/></>
        <>Female<input type="checkbox" id="female" onChange={getGender}/></>
-       <>Undefined<input type="checkbox" id="undefined" onChange={getGender}/></> */}
+       <>Undefined<input type="checkbox" id="undefined" onChange={getGender}/></>
        
        <ul>
             {contact.map(item => <Contact key={item.id} {...item} />)}
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,18 +1,14 @@
 import React from 'react';
 
-
 export default function Contact ({id, firstName, lastName, phone, gender}) {
     return(
         <li>
-        <div className="name">
-            <span className="firstName">{firstName}&emsp;</span>
-            
-            <span className="lastName">{lastName}</span>
-            <span className="gender"><img src={gender} alt=""></img></span>
-            <div className="phone">{phone}</div>
-        </div>
-        
-        
-    </li>
+            <div className="name">
+                <span className="firstName">{firstName}&emsp;</span>
+                <span className="lastName">{lastName}</span>
+                <span className="gender"><img src={gender} alt=""></img></span>
+                <div className="phone">{phone}</div>
+            </div>
+        </li>
     )
 }
