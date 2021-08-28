cd6a3d00b93485d03681dccf511259f64367dc1c
Volodymyr-Dr <vv.dr.pvlk@gmail.com>
2021-08-28 20:48:48 +0300

    formatting

--- src/App.js
+++ src/App.js
@@ -24,18 +24,6 @@ function App() {
     setSearchTerm(event.target.value)
   };
  
-  const getGender = () => {
-    if (document.getElementById("male").checked) {
-      setContact(contact.filter(item => item.gender === "https://image.flaticon.com/icons/png/512/1340/1340619.png"));
-    }
-    if (document.getElementById("female").checked) {
-      setContact(contact.filter(item => item.gender === "https://image.flaticon.com/icons/png/512/766/766514.png"));
-    }
-    if (document.getElementById("undefined").checked) {
-      setContact(contact.filter(item => item.gender === "http://pngimg.com/uploads/question_mark/question_mark_PNG91.png"));
-    }
-  };
-  
     return (
     <div className="App">
       <h1>Contacts</h1>
@@ -47,11 +35,6 @@ function App() {
             onChange={handleSearchChange}
           />
        </div>
-
-       <>Male<input type="checkbox" id="male" onChange={getGender}/></>
-       <>Female<input type="checkbox" id="female" onChange={getGender}/></>
-       <>Undefined<input type="checkbox" id="undefined" onChange={getGender}/></>
-       
        <ul>
             {contact.map(item => <Contact key={item.id} {...item} />)}
         </ul>
